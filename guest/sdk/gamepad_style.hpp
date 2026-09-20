1b244aa570eeed412ced1c695c6a9803b5727bff
Terrence <terrence@tenclass.com>
2026-09-21 05:18:57 +0800

    style(gamepad): unify idle glyph and rim appearance

--- guest/sdk/gamepad_style.hpp
+++ guest/sdk/gamepad_style.hpp
@@ -10,12 +10,12 @@ namespace micropixel {
 struct GamepadButtonStyle final {
     Color fill{Color::Rgb(24U, 24U, 24U)};
     uint8_t fill_opacity{0U};
-    Color rim{Color::White()};
-    uint8_t rim_opacity{64U};
+    Color rim{Color::Rgb(112U, 112U, 112U)};
+    uint8_t rim_opacity{255U};
     Color pressed_fill{Color::Rgb(32U, 32U, 32U)};
     uint8_t pressed_fill_opacity{128U};
-    Color glyph{Color::Rgb(112U, 112U, 112U)};
-    uint8_t glyph_opacity{255U};
+    Color glyph{rim};
+    uint8_t glyph_opacity{rim_opacity};
 };
 
 }  // namespace micropixel
